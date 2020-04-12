5c91c1e925e8ef67256c27a6f4ec4c32b997df7d
kubal97 <jakubl1997@interia.pl>
2020-04-12 15:05:22 +0200

    Updates

---
+++ precache-manifest.f5dbd5559f3b73f547e6b5cf470e24bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20da5f28445ce01eafce0a3b401410af",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "53695c8b69471ca6e708",
+    "url": "/pokedex/static/css/main.57f462e3.chunk.css"
+  },
+  {
+    "revision": "ce8387ba89ef5b621c4f",
+    "url": "/pokedex/static/js/2.e9f2dd0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.e9f2dd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53695c8b69471ca6e708",
+    "url": "/pokedex/static/js/main.7ea0b50c.chunk.js"
+  },
+  {
+    "revision": "23e4c6896b4e85d076c4",
+    "url": "/pokedex/static/js/runtime-main.3f186e6a.js"
+  }
+]);
